8e7dbb17a40e11ae02cb42c047253c67820a3a58
Martin Topfstedt <martin.topfstedt@codinion.com>
2021-12-08 20:21:08 +0100

    fixed namespaces and remove comments

--- src/FontAwesome6.Svg.Net/XmlNamespace.cs
+++ src/FontAwesome6.Svg.Net/XmlNamespace.cs
@@ -3,4 +3,4 @@
 [assembly: XmlnsPrefix("http://schemas.fontawesome.com/icons/svg", "fa6")]
 [assembly: XmlnsDefinition("http://schemas.fontawesome.com/icons/svg", "FontAwesome6.Svg")]
 [assembly: XmlnsDefinition("http://schemas.fontawesome.com/icons/svg", "FontAwesome6.Svg.Converters")]
-[assembly: XmlnsDefinition("http://schemas.fontawesome.com/icons/svg", "FontAwesome6.Svg.AttachedProperties")]
+[assembly: XmlnsDefinition("http://schemas.fontawesome.com/icons/svg/properties", "FontAwesome6.Svg.AttachedProperties")]
